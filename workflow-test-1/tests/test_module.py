c11d9931e521da3c4bc10355d28d04d16c5d941f
dkh634 <karinto634@outlook.jp>
2025-06-27 23:55:42 +0900

    修正

--- workflow-test-1/tests/test_module.py
+++
@@ -1,8 +0,0 @@
-from src.module import foo
-
-def test_foo():
-    assert foo(1, 2) == 3
-    assert foo(-1, 1) == 0
-    assert foo(0, 0) == 0
-    assert foo(100, 200) == 300
-    assert foo(-100, -200) == -300
